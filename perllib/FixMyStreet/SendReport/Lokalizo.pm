5e338e2dd7f0a5a71e815934b682bd04087a5607
Altin Ukshini <altin.ukshini@gmail.com>
2017-03-03 17:02:45 +0100

    Lokalizo sending method changes

--- perllib/FixMyStreet/SendReport/Lokalizo.pm
+++ perllib/FixMyStreet/SendReport/Lokalizo.pm
@@ -9,4 +9,60 @@ sub send_from {
     return [ FixMyStreet->config('CONTACT_EMAIL'), FixMyStreet->config('CONTACT_NAME') ];
 }
 
+sub reply_to {
+    my ( $self, $row ) = @_;
+    return [ $row->user->email, $row->name ];
+}
+
+sub send {
+    my $self = shift;
+    my ( $row, $h ) = @_;
+
+    my $recips = $self->build_recipient_list( $row, $h );
+
+    # on a staging server send emails to ourselves rather than the bodies
+    if (FixMyStreet->config('STAGING_SITE') && !FixMyStreet->config('SEND_REPORTS_ON_STAGING') && !FixMyStreet->test_mode) {
+        $recips = 1;
+        @{$self->to} = [ $row->user->email, $self->to->[0][1] || $row->name ];
+    }
+
+    unless ($recips) {
+        $self->error( 'No recipients' );
+        return 1;
+    }
+
+    my ($verbose, $nomail) = CronFns::options();
+    my $cobrand = FixMyStreet::Cobrand->get_class_for_moniker($row->cobrand)->new();
+    my $params = {
+        To => $self->to,
+        From => $self->send_from( $row ),
+    };
+
+    $cobrand->munge_sendreport_params($row, $h, $params) if $cobrand->can('munge_sendreport_params');
+
+    $params->{Bcc} = $self->bcc if @{$self->bcc};
+
+    my $sender = sprintf('<fms-%s@%s>',
+        FixMyStreet::Email::generate_verp_token('report', $row->id),
+        FixMyStreet->config('EMAIL_DOMAIN')
+    );
+
+    if (FixMyStreet::Email::test_dmarc($params->{From}[0])) {
+        $params->{'Reply-To'} = $self->reply_to( $row );
+        $params->{From} = [ $sender, $params->{From}[1] ];
+    }
+
+    my $result = FixMyStreet::Email::send_cron($row->result_source->schema,
+        $self->get_template($row), $h,
+        $params, $sender, $nomail, $cobrand, $row->lang);
+
+    unless ($result) {
+        $self->success(1);
+    } else {
+        $self->error( 'Failed to send email' );
+    }
+
+    return $result;
+}
+
 1;
